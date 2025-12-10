50892042d7084e8f43169000cf2a16823471020f
Ricz Attila <ricza@kkszki.hu>
2025-12-10 10:38:26 +0100

    .txt file beolvasása, és megjelenítése DataGridben

--- DolgozatWPF/MainWindow.xaml.cs
+++ DolgozatWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -33,6 +34,16 @@ namespace DolgozatWPF
         public MainWindow()
         {
             InitializeComponent();
+            var sorok = File.ReadAllLines("dolgozatok.txt").Skip(1);
+            foreach(string s in sorok)
+            {
+                string[] darabok = s.Split(';');
+                string neve = darabok[0];
+                int eletkora = Convert.ToInt32(darabok[1]);
+                int pontszama = Convert.ToInt32(darabok[2]);
+                dolgozatok.Add(new Dolgozat(neve, eletkora, pontszama));
+            }
+            dataGrid.ItemsSource = dolgozatok;
         }
     }
 }
