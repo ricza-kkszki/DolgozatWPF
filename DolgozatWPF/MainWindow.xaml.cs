ee2bef175755639233624fa750a0034b3ff9f883
Ricz Attila <ricza@kkszki.hu>
2025-12-10 10:32:03 +0100

    .txt file importálása + dolgozat osztály és lista létrehozása

--- DolgozatWPF/MainWindow.xaml.cs
+++ DolgozatWPF/MainWindow.xaml.cs
@@ -14,8 +14,22 @@ namespace DolgozatWPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    public class Dolgozat
+    {
+        public string Nev { get; set; }
+        public int Eletkor { get; set; }
+        public int Pontszam { get; set; }
+        public Dolgozat(string nev, int eletkor, int pontszam)
+        {
+            Nev = nev;
+            Eletkor = eletkor;
+            Pontszam = pontszam;
+        }
+    }
     public partial class MainWindow : Window
     {
+        public List<Dolgozat> dolgozatok = new List<Dolgozat>();
         public MainWindow()
         {
             InitializeComponent();
